81af70ff33856db1e31b5bd1ee4c50e6e0df5b77
Ashutosh Patro <ashutosh@itobuz.com>
2023-03-01 18:41:39 +0530

    second commit

--- js/script.js
+++ js/script.js
@@ -1,11 +1,22 @@
-function getWeatherData() {
-    const response = fetch('http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=London&aqi=no')
-        .then(
-            function (res) {
-                return res.json();
-            });
-    console.log(response, location)
+async function getInfo() {
+    response = await fetch(
+        "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=Kolkata&aqi=no"
+    )
+        .then((data) => {
+            return data.json();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    console.log(response);
+    addData();
 }
-getWeatherData()
 
+getInfo();
+
+function addData() {
+    document.querySelector('.header').innerHTML = `${response.location.name}`
+    document.querySelector('.temperature').innerHTML += response.current.temp_c + '<sup>o</sup>'
+    document.querySelector('.feels-like').innerHTML += response.current.feelslike_c + '<sup>o</sup>'
+}
 
