6da4cb79a876575d1b71d6f4fa81f44e42106f07
Ashutosh Patro <ashutosh@itobuz.com>
2023-03-15 12:30:59 +0530

    15th March commit

--- js/script.js
+++ js/script.js
@@ -1,20 +1,27 @@
-const locationName = document.querySelector('.location')
-const image = document.querySelector('.day-night-image')
-const weatherImage = document.querySelector('.weather-image')
-const textWeather = document.querySelector('.weather-text')
+const locationName = document.querySelector(".location");
+const image = document.querySelector(".day-night-image");
+const weatherImage = document.querySelector(".weather-image");
+const textWeather = document.querySelector(".weather-text");
+const inputDrop = document.querySelector(".location-list");
+const citySelect = document.querySelectorAll(".location-list li");
+
 
 async function getInfo() {
     response = await fetch(
-        "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" + `${locationName.value}` + "&aqi=no"
+        "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" +
+        `${locationName.value}` +
+        "&aqi=no"
     )
-        .then((data) => data.json()
-        )
+        // response = await fetch(
+        //     "http://localhost:5000/?location=Kolkata"
+        // )
+        .then((data) => data.json())
         .catch((err) => {
             console.log(err);
         });
 
     console.log(response);
-    setImage(response)
+    setImage(response);
     addData();
 }
 
@@ -22,44 +29,62 @@ function setImage() {
     let weatherValue = response.current.condition.text;
     let dayValue = response.current.is_day;
     if (dayValue == 1) {
-        image.setAttribute('src', './images/sun.png');
-    }
-    else {
-        image.setAttribute('src', './images/moon.png');
+        image.setAttribute("src", "./images/sun.png");
+    } else {
+        image.setAttribute("src", "./images/moon.png");
     }
 
     if (weatherValue == "Mist") {
-        weatherImage.setAttribute('src', './images/mist.png')
-        textWeather.innerHTML = 'Weather:' + weatherValue;
-    }
-    else if (weatherValue == "Overcast") {
-        weatherImage.setAttribute('src', './images/overcast.png')
-        textWeather.innerHTML = 'Weather:' + weatherValue;
-    }
-    else if (weatherValue == "Light snow") {
-        weatherImage.setAttribute('src', './images/light-snow.jpg')
-        textWeather.innerHTML = 'Weather:' + weatherValue;
-    }
-    else if (weatherValue == "Partly cloudy") {
-        weatherImage.setAttribute('src', './images/partly-cloudy.png')
-        textWeather.innerHTML = 'Weather:' + weatherValue;
-    }
-    else if (weatherValue == "Sunny") {
-        weatherImage.setAttribute('src', './images/sun.png')
-        textWeather.innerHTML = 'Weather:' + weatherValue;
+        weatherImage.setAttribute("src", "./images/mist.png");
+        textWeather.innerHTML = "Weather:" + weatherValue;
+    } else if (weatherValue == "Overcast") {
+        weatherImage.setAttribute("src", "./images/overcast.png");
+        textWeather.innerHTML = "Weather:" + weatherValue;
+    } else if (weatherValue == "Light snow") {
+        weatherImage.setAttribute("src", "./images/light-snow.jpg");
+        textWeather.innerHTML = "Weather:" + weatherValue;
+    } else if (weatherValue == "Partly cloudy") {
+        weatherImage.setAttribute("src", "./images/partly-cloudy.png");
+        textWeather.innerHTML = "Weather:" + weatherValue;
+    } else if (weatherValue == "Sunny") {
+        weatherImage.setAttribute("src", "./images/sun.png");
+        textWeather.innerHTML = "Weather:" + weatherValue;
     }
 }
 
 function addData() {
-    document.querySelector('.location').innerHTML = `${locationName}`
-    document.querySelector('.temperature').innerHTML = response.current.temp_c + '<sup>o</sup>'
-    document.querySelector('.feels-like').innerHTML = 'Feels ' + response.current.feelslike_c + '<sup>o</sup>'
-
+    locationName.innerHTML = `${locationName}`;
+    document.querySelector(".temperature").innerHTML =
+        response.current.temp_c + "<sup>o</sup>C";
+    document.querySelector(".feels-like").innerHTML =
+        "Feels " + response.current.feelslike_c + "<sup>o</sup>C";
 }
 
-locationName.addEventListener('keyup', (e) => {
-    if (e.key == 'Enter') {
+locationName.addEventListener("keyup", (e) => {
+    if (e.key == "Enter") {
         console.log(e);
-        getInfo()
+        getInfo();
+    }
+});
+
+locationName.onclick = () => {
+    console.log(inputDrop);
+    inputDrop.classList.toggle('hide')
+}
+
+citySelect.forEach((ele) => {
+    ele.addEventListener("click", () => {
+        locationName.value = ele.textContent;
+        inputDrop.classList.toggle('hide');
+        getInfo();
+    });
+})
+
+window.addEventListener('click', (e) => {
+    if (e.target == locationName) {
+        inputDrop.classList.remove('hide')
+    }
+    else {
+        inputDrop.classList.add('hide')
     }
 })
