522fe2c8a74210bceb9dd62676540f73086d6870
Ashutosh Patro <ashutosh@itobuz.com>
2023-03-22 11:26:50 +0530

    updated 22nd march

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
-const locationName = document.querySelector(".location");
+const locationName = document.querySelector(".location-input");
 const image = document.querySelector(".day-night-image");
 const weatherImage = document.querySelector(".weather-image");
 const textWeather = document.querySelector(".weather-text");
 const inputDrop = document.querySelector(".location-list");
-const citySelect = document.querySelectorAll(".location-list li");
+// const citySelect = document.querySelectorAll(".location-list");
 
+let allCities = []
 
 async function getInfo() {
+    // response = await fetch(
+    //     "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" +
+    //     `${locationName.value}` +
+    //     "&aqi=no"
+    // ) 
     response = await fetch(
-        "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" +
-        `${locationName.value}` +
-        "&aqi=no"
+        `http://localhost:5000/get?location=${locationName.value}`
     )
-        // response = await fetch(
-        //     "http://localhost:5000/?location=Kolkata"
-        // )
         .then((data) => data.json())
         .catch((err) => {
             console.log(err);
         });
-
-    console.log(response);
     setImage(response);
-    addData();
+    addData(response);
 }
 
 function setImage() {
-    let weatherValue = response.current.condition.text;
-    let dayValue = response.current.is_day;
+    let weatherValue = response.condition.text;
+    let dayValue = response.condition.isDay;
     if (dayValue == 1) {
         image.setAttribute("src", "./images/sun.png");
     } else {
@@ -36,28 +35,29 @@ function setImage() {
 
     if (weatherValue == "Mist") {
         weatherImage.setAttribute("src", "./images/mist.png");
-        textWeather.innerHTML = "Weather:" + weatherValue;
+        textWeather.innerHTML = "Weather: " + weatherValue;
     } else if (weatherValue == "Overcast") {
         weatherImage.setAttribute("src", "./images/overcast.png");
-        textWeather.innerHTML = "Weather:" + weatherValue;
+        textWeather.innerHTML = "Weather: " + weatherValue;
     } else if (weatherValue == "Light snow") {
         weatherImage.setAttribute("src", "./images/light-snow.jpg");
-        textWeather.innerHTML = "Weather:" + weatherValue;
+        textWeather.innerHTML = "Weather: " + weatherValue;
     } else if (weatherValue == "Partly cloudy") {
         weatherImage.setAttribute("src", "./images/partly-cloudy.png");
-        textWeather.innerHTML = "Weather:" + weatherValue;
-    } else if (weatherValue == "Sunny") {
-        weatherImage.setAttribute("src", "./images/sun.png");
-        textWeather.innerHTML = "Weather:" + weatherValue;
+        textWeather.innerHTML = "Weather: " + weatherValue;
     }
+    // else if (weatherValue == "Sunny") {
+    //     weatherImage.setAttribute("src", "./images/sun.png");
+    //     textWeather.innerHTML = "Weather:" + weatherValue;
+    // }
 }
 
-function addData() {
+function addData(response) {
     locationName.innerHTML = `${locationName}`;
     document.querySelector(".temperature").innerHTML =
-        response.current.temp_c + "<sup>o</sup>C";
+        response.tempC + "<sup>o</sup>C";
     document.querySelector(".feels-like").innerHTML =
-        "Feels " + response.current.feelslike_c + "<sup>o</sup>C";
+        "Feels " + response.feelsLikeC + "<sup>o</sup>C";
 }
 
 locationName.addEventListener("keyup", (e) => {
@@ -68,17 +68,24 @@ locationName.addEventListener("keyup", (e) => {
 });
 
 locationName.onclick = () => {
-    console.log(inputDrop);
     inputDrop.classList.toggle('hide')
 }
 
-citySelect.forEach((ele) => {
-    ele.addEventListener("click", () => {
-        locationName.value = ele.textContent;
-        inputDrop.classList.toggle('hide');
-        getInfo();
+inputDrop.addEventListener("click", (e) => {
+    locationName.value = e.target.textContent;
+    inputDrop.classList.toggle('hide');
+    getInfo();
+});
+
+
+function listCities() {
+    allCities.forEach((city) => {
+        let li = document.createElement("li");
+        li.textContent = city;
+        li.setAttribute("class", "location-hover")
+        inputDrop.appendChild(li);
     });
-})
+}
 
 window.addEventListener('click', (e) => {
     if (e.target == locationName) {
@@ -88,3 +95,26 @@ window.addEventListener('click', (e) => {
         inputDrop.classList.add('hide')
     }
 })
+
+async function getCities() {
+    console.log("hi");
+    await fetch(
+        `http://localhost:5000/city`
+    )
+        .then((data) => data.json())
+        .then((data) => {
+            data.forEach((item) => {
+                allCities.push(item)
+            })
+        })
+        .then(() => {
+            listCities()
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+(() => {
+    getCities()
+})()
